e317213b38914c70a8a885d5022ee67b44989b11
Howard Mao <zhehao.mao@gmail.com>
2019-09-02 19:59:08 -0700

    [debugging] run test executable manually to see error output

--- .circleci/run-firesim-tests.sh
+++ .circleci/run-firesim-tests.sh
@@ -13,7 +13,14 @@ export FIRESIM_ENV_SOURCED=1
 SIMULATION_ARGS="${mapping[$1]}"
 
 run_test_suite () {
-    make -C $LOCAL_FIRESIM_DIR $SIMULATION_ARGS run-${1}-tests-fast
+    $SIMULATION_ARGS
+    TRIPLET=${DESIGN}-${TARGET_CONFIG}-${PLATFORM_CONFIG}
+    OUTPUT_DIR=$LOCAL_FIRESIM_DIR/output/f1/${TRIPLET}
+    GENERATED_DIR=$LOCAL_FIRESIM_DIR/generated-src/f1/${TRIPLET}
+    ln -fs $RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64ud-v-fcvt ${OUTPUT_DIR}/rv64ud-v-fcvt
+    cd $LOCAL_FIRESIM_DIR/generated-src/f1/${TRIPLET}
+    ./V${DESIGN} ${OUTPUT_DIR}/rv64ud-v-fcvt $(cat runtime.conf) +nic-loopback0 +linklatency0=6405 +blkdev-in-mem0=128 +max-cycles=100000 
+    #make -C $LOCAL_FIRESIM_DIR $SIMULATION_ARGS run-${1}-tests-fast
 }
 
 run_test_suite regression
